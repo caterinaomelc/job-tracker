15a7fc8b5654ef8e76664046227aaff46d82f031
caterinaomelc <236698397+caterinaomelc@users.noreply.github.com.>
2026-05-12 03:12:31 +0300

    feat: log CompanyController and GlobalExceptionHandler

--- src/main/java/com/jobtracker/application/controller/CompanyController.java
+++ src/main/java/com/jobtracker/application/controller/CompanyController.java
@@ -10,9 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-
 
 
 @Slf4j
@@ -23,30 +22,41 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
+
     @PostMapping
     public ResponseEntity<CompanyResponse> createCompany(@Valid @RequestBody CompanyRequest request) {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Company {} is created by user {} ", request.name(), currentUserName);
         return ResponseEntity.status(HttpStatus.CREATED).body(companyService.addCompany(request));
 
     }
 
     @GetMapping
     public ResponseEntity<?> getAllCompanies(Pageable pageable) {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Companies are getting by user {} ", currentUserName);
         return ResponseEntity.ok(companyService.getAllCompanies(pageable));
     }
 
     @GetMapping("${end.points.id}")
     public ResponseEntity<CompanyResponse> getCompanyById(@PathVariable Long id) {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Company {} is getting by user {} ", id, currentUserName);
         return ResponseEntity.ok().body(companyService.getCompanyById(id));
     }
 
     @PutMapping("${end.points.id}")
     public ResponseEntity<CompanyResponse> updateCompany(@Valid @RequestBody CompanyRequest request,
-                                         @PathVariable Long id) {
+                                                         @PathVariable Long id) {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Company {} is updating by user {} ", id, currentUserName);
         return ResponseEntity.ok().body(companyService.updateCompany(request, id));
     }
 
     @DeleteMapping("${end.points.id}")
     public ResponseEntity<CompanyResponse> deleteCompany(@PathVariable Long id) {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Company {} is deleting by user {} ", id, currentUserName);
         companyService.deleteCompany(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/jobtracker/application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobtracker/application/exception/GlobalExceptionHandler.java
@@ -1,43 +1,71 @@
 package com.jobtracker.application.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<ErrorResponse> handleInvalidData(InvalidDataException e) {
         ErrorResponse error = new ErrorResponse(400, e.getMessage(), "BAD_REQUEST", LocalDateTime.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+            log.error("Attempt by user {} has failed. InvalidDataException: {}", currentUserName, e.getMessage());
+        } else {
+            log.error("InvalidDataException: ", e);
+        }
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException e) {
         ErrorResponse error = new ErrorResponse(400, e.getMessage(), "BAD_REQUEST", LocalDateTime.now());
+        log.error("MethodArgumentNotValidException: ", e);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
         ErrorResponse error = new ErrorResponse(404, e.getMessage(), "NOT_FOUND", LocalDateTime.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+            log.error("Attempt by user {} has failed. NotFoundException: {}", currentUserName, e.getMessage());
+        } else {
+            log.error("NotFoundException: ", e);
+        }
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CompanyAlreadyExists.class)
     public ResponseEntity<ErrorResponse> handleCompanyAlreadyExists(CompanyAlreadyExists e) {
         ErrorResponse error = new ErrorResponse(409, e.getMessage(), "CONFLICT", LocalDateTime.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+            log.error("Attempt by user {} has failed. CompanyAlreadyExists: {}", currentUserName, e.getMessage());        } else {
+            log.error("CompanyAlreadyExists: ", e);
+        }
+
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneral(Exception e) {
         ErrorResponse error = new ErrorResponse(500, "Internal serves error", "INTERNAL_SERVER_ERROR", LocalDateTime.now());
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+            log.error("User {} has encountered a server error. Internal serves error", currentUserName);
+        } else {
+            log.error("Internal serves error ", e);
+        }
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
